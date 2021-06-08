d35ac81dc94a1c8cfc736701334c66bf4571ccce
rodrigo martinez <rmartinez@MacBook-Pro-de-rodrigo.local>
2021-06-08 12:49:57 -0300

    Added Project Structure

---
+++ src/test/java/com/w2m/superheros/SuperherosApplicationTests.java
@@ -0,0 +1,13 @@
+package com.w2m.superheros;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SuperherosApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
