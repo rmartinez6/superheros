659230403ab298b647585f2d43629b6ccdf3f05d
rodrigo martinez <rmartinez@MacBook-Pro-de-rodrigo.local>
2021-06-10 19:37:54 -0300

    Added implementation update Superhero into SuperHeroService

--- src/main/java/com/w2m/superheros/service/SuperHeroService.java
+++ src/main/java/com/w2m/superheros/service/SuperHeroService.java
@@ -1,6 +1,7 @@
 package com.w2m.superheros.service;
 
 import com.w2m.superheros.domain.SuperHero;
+import com.w2m.superheros.exception.ResourceNotFoundException;
 import com.w2m.superheros.repository.SuperHeroRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
@@ -23,6 +24,14 @@ public class SuperHeroService {
 
     public List<SuperHero> findByName(String name) {
         return superHeroRepository.findByNameContainingIgnoreCase(name);
+    }
+
+    public SuperHero update(Long id, SuperHero superHeroDetails) throws Exception {
+        SuperHero superHero = superHeroRepository.findById(id).get();
+        superHero.setName(superHeroDetails.getName());
+        superHero.setFullName(superHeroDetails.getFullName());
+        superHero.setPlaceOfBirth(superHeroDetails.getPlaceOfBirth());
+        return superHeroRepository.save(superHero);
     }
 
 }
