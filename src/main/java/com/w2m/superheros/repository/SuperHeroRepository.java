16f4d9597c14f1b5e7f4192e65ab44c149e4e777
rodrigo martinez <rmartinez@MacBook-Pro-de-rodrigo.local>
2021-06-10 13:27:33 -0300

    Added method findByContainName into repository

--- src/main/java/com/w2m/superheros/repository/SuperHeroRepository.java
+++ src/main/java/com/w2m/superheros/repository/SuperHeroRepository.java
@@ -3,7 +3,11 @@ package com.w2m.superheros.repository;
 import com.w2m.superheros.domain.SuperHero;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
+import java.util.List;
 
 @Repository()
 public interface SuperHeroRepository extends JpaRepository<SuperHero, Long> {
+
+    List<SuperHero> findByNameContainingIgnoreCase(String name);
+
 }
