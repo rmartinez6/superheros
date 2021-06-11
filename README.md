e62e5d38684000abf9559c6ff29462720cb46042
rodrigo martinez <rmartinez@MacBook-Pro-de-rodrigo.local>
2021-06-11 00:59:57 -0300

    Added Swagger Documentation

--- README.md
+++ README.md
@@ -1,3 +1,6 @@
 # W2M Challenge
 ## Prueba técnica Spring Boot
-### API que permita hacer un mantenimiento de súper héroes.
+### API que permita hacer mantenimiento de súper héroes.
+
+#### Documentacion: 
+Swagger --> http://localhost:8080/swagger-ui/index.htm
---
+++ src/main/java/com/w2m/superheros/configuration/SpringFoxConfig.java
@@ -0,0 +1,34 @@
+package com.w2m.superheros.configuration;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import springfox.documentation.builders.ApiInfoBuilder;
+import springfox.documentation.builders.PathSelectors;
+import springfox.documentation.builders.RequestHandlerSelectors;
+import springfox.documentation.service.ApiInfo;
+import springfox.documentation.service.Contact;
+import springfox.documentation.spi.DocumentationType;
+import springfox.documentation.spring.web.plugins.Docket;
+
+@Configuration
+public class SpringFoxConfig {
+    @Bean
+    public Docket api() {
+        return new Docket(DocumentationType.SWAGGER_2)
+                .select()
+                .apis(RequestHandlerSelectors.basePackage("com.w2m.superheros.controller"))
+                .paths(PathSelectors.any())
+                .build()
+                .apiInfo(getApiInfo());
+    }
+
+    private ApiInfo getApiInfo() {
+        return new ApiInfoBuilder()
+                .title("Spring Boot REST API by SuperHeros")
+                .version("1.0.0")
+                .license("Apache License Version 2.0")
+                .licenseUrl("https://www.apache.org/licenses/LICENSE-2.0\"")
+                .contact(new Contact("Rodrigo Martinez", "https://github.com/rmartinez6/superheros", "rdmartinez6@gmail.com"))
+                .build();
+    }
+}
